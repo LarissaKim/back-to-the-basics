a1ead2e82c0784e242d6dda333e44bf859267682
Larissa Kim <lrsslsrk@gmail.com>
2022-02-03 16:05:31 -0700

    feat(board): add game completion functionality

--- src/index.js
+++ src/index.js
@@ -21,6 +21,11 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
   }
@@ -30,7 +35,12 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    const winner = calculateWinner(this.state.squares);
+
+    let status;
+    winner
+      ? (status = `Winner: ${winner}`)
+      : (status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`);
 
     return (
       <div>
